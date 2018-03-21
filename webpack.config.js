7f0e55c1aad0ca936503223758f59344e1d567bc
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-03-22 09:30:43 +1000

    Added new dependencies for running phaser and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -16,10 +17,18 @@ module.exports = {
                         options: { minimize: true }
                     }
                 ]
+            },
+            {
+                test: [ /\.vert$/, /\.frag$/ ],
+                loader: 'raw-loader'
             }
         ]
     },
     plugins: [
+        new webpack.DefinePlugin({
+            CANVAS_RENDERER: JSON.stringify(true),
+            WEBGL_RENDERER: JSON.stringify(true)
+        }),
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: './index.html'
