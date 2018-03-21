535a6b5ab5a0c22afbb5951d1b5d5b2e9311cf27
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-03-22 09:29:56 +1000

    Initialise the phaser game

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-console.log('Entry file');
-const myConst = 3.1415926535897;
-const es6Function = () => console.log(myConst);
+import 'phaser';
 
-window.es6Function = es6Function;
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#55CC55',
+    pixelArt: true,
+    parent: 'gameDiv',
+    scene: [ ],
+    title: 'Speedrunner 2.0',
+    banner: {
+        text: '#191970',
+        background: [
+            '#555555',
+            '#20B2AA',
+            '#9ACD32',
+            '#B8860B',
+            '#F5F5DC'
+        ],
+        hidePhaser: true
+    }
+};
+
+/* eslint-disable no-unused-vars */
+const game = new Phaser.Game(config);
