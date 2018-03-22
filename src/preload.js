0c2ec0889e8b7e71eb5ab2d65a6efab8f7afdc4e
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-03-22 16:10:08 +1000

    First commit

---
+++ src/preload.js
@@ -0,0 +1,20 @@
+class Preload extends Phaser.Scene {
+    constructor () {
+        super({
+            key: 'Prelaod',
+            file: ''
+        });
+    }
+
+    preload () {
+
+    }
+
+    create () {
+
+    }
+
+
+}
+
+export default Preload;
