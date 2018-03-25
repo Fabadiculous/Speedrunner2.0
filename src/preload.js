ca2a83f624e2907531908dff0824f8bc0a7f9d8c
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-03-25 20:26:08 +1000

    Testing tile maps

--- src/preload.js
+++ src/preload.js
@@ -9,6 +9,7 @@ class Preload extends Phaser.Scene {
     }
 
     preload () {
+        // Path for images and sprite sheets
         this.load.setPath('../assets/images/');
 
         this.load.image([
@@ -26,19 +27,32 @@ class Preload extends Phaser.Scene {
             { key: 'flyEnemy', file: 'FlyingEnemy.png', config: {frameWidth: 25, frameHeight: 23} },
             { key: 'movingPlatform', file: 'MovingPlatform.png', config: {frameWidth: 64, frameHeight: 32} },
             { key: 'player', file: 'Player.png', config: {frameWidth: 16, frameHeight: 32} },
-            { key: 'groundTile', file: 'GroundTiles.png', config: {frameWidth: 32, frameHeight: 32} },
+            { key: 'groundTiles', file: 'GroundTiles.png', config: {frameWidth: 64, frameHeight: 64} },
             { key: 'soundBtn', file: 'SoundButtons.png', config: {frameWidth: 64, frameHeight: 64} },
             { key: 'star', file: 'Stars.png', config: {frameWidth: 20, frameHeight: 20} },
             { key: 'lvlThumbnail', file: 'Thumbnail.png', config: {frameWidth: 64, frameHeight: 64} }
         ]);
 
+        // Path for audio
         this.load.setPath('../assets/sounds');
 
         this.load.audio('getStar', [ 'getStar.mp3', 'getStar.ogg' ]);
+
+        // Path for levels
+        this.load.setPath('../assets/levels');
+        this.load.tilemapTiledJSON('level1', 'Level1.json');
+
     }
 
     create () {
         this.add.image(400, 300, 'preloadBar');
+
+        this.map = this.make.tilemap({ key: 'level1' });
+
+        let tiles = this.map.addTilesetImage('groundTiles');
+
+        let layer = this.map.createStaticLayer('GroundLayer', tiles);
+        console.log(layer);
     }
 
 
