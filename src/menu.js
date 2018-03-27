f127afdbde14c5320359524868c59eeb1c37e27c
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-03-27 15:56:46 +1000

    Add menu title and test buttons

--- src/menu.js
+++ src/menu.js
@@ -1,14 +1,48 @@
+import Button from './Button';
+
 class Menu extends Phaser.Scene {
     constructor () {
         super({ key: 'menu' });
     }
 
+    init () {
+        this.scene.bringToTop();
+    }
+
     create () {
-        this.scene.start('playGame');
+        let menuText = {
+            x: this.sys.game.config.width / 2,
+            y: 0,
+            text: 'SPEEDRUNNER',
+            style: {
+                fontSize: '64px',
+                fontFamily: 'Arial',
+                color: '#FFD700',
+                align: 'center'
+            }
+        };
+
+        this.make.text(menuText).setOrigin(0.5, 0);
+
+        this.createButton(100, 100, function () { console.log('Test'); }, this);
+
+        let button = new Button(200, 100, function () { console.log('Test2'); }, this);
     }
 
     update () {
 
+    }
+
+    createButton (x, y, callback, context, hover = 1, onDown = 2) {
+        let button = context.add.image(x, y, 'button').setInteractive();
+        button.on('pointerover', () => button.setFrame(hover));
+        button.on('pointerout', () => button.setFrame(0));
+        button.on('pointerdown', () => {
+            button.setFrame(onDown);
+            callback();
+        });
+
+        return button;
     }
 }
 
