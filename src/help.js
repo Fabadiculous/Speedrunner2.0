b1aed1dfa6ad825ef33be50e98462aa0e44e4ebe
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-03-31 17:06:18 +1000

    Text for help scene

--- src/help.js
+++ src/help.js
@@ -6,7 +6,24 @@ class Help extends Phaser.Scene {
         });
     }
 
+    init () {
+        // this.scene.bringToTop();
+    }
+
     create () {
+        let helpText = {
+            x: this.registry.get('width') / 2,
+            y: 0,
+            text: 'HELP',
+            style: {
+                fontSize: '64px',
+                fontFamily: 'Arial',
+                color: '#FFD700',
+                align: 'center'
+            }
+        };
+        this.make.text(helpText).setOrigin(0.5, 0);
+
 
     }
 }
