446cfbbd92816d839fe49ba4ee9855115cbbfb52
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-06 08:11:39 +1000

    Added animations for spritesheets

--- src/animations.js
+++ src/animations.js
@@ -10,12 +10,52 @@ class CreateAnims extends Phaser.Scene {
         console.log('Creating animations');
         let playerConfig = {
             key: 'run',
-            frames: this.anims.generateFrameNumbers('player'),
+            frames: this.anims.generateFrameNumbers('player', {start: 0, end: 13}),
+            frameRate: 20,
+            repeat: -1
+        };
+
+        let starConfig = {
+            key: 'shine',
+            frames: this.anims.generateFrameNumbers('star'),
+            frameRate: 20,
+            repeat: -1
+        };
+
+        let coinConfig = {
+            key: 'spin',
+            frames: this.anims.generateFrameNumbers('coin'),
+            frameRate: 20,
+            repeat: -1
+        };
+
+        let platformConfig = {
+            key: 'hover',
+            frames: this.anims.generateFrameNumbers('movingPlatform'),
+            frameRate: 20,
+            repeat: -1
+        };
+
+        let flyConfig = {
+            key: 'fly',
+            frames: this.anims.generateFrameNumbers('flyEnemy'),
+            frameRate: 20,
+            repeat: -1
+        };
+
+        let snakeConfig = {
+            key: 'move',
+            frames: this.anims.generateFrameNumbers('snake'),
             frameRate: 20,
             repeat: -1
         };
 
         this.anims.create(playerConfig);
+        this.anims.create(starConfig);
+        this.anims.create(coinConfig);
+        this.anims.create(platformConfig);
+        this.anims.create(flyConfig);
+        this.anims.create(snakeConfig);
 
         // End the Scene
         this.scene.remove('createAnims');
