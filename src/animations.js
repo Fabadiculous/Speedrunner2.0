3093cda17a7325f5cc9f27a7150edd50831903df
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-06 08:16:05 +1000

    Fix some animations

--- src/animations.js
+++ src/animations.js
@@ -15,17 +15,11 @@ class CreateAnims extends Phaser.Scene {
             repeat: -1
         };
 
-        let starConfig = {
-            key: 'shine',
-            frames: this.anims.generateFrameNumbers('star'),
-            frameRate: 20,
-            repeat: -1
-        };
-
         let coinConfig = {
             key: 'spin',
             frames: this.anims.generateFrameNumbers('coin'),
             frameRate: 20,
+            yoyo: true,
             repeat: -1
         };
 
@@ -40,18 +34,18 @@ class CreateAnims extends Phaser.Scene {
             key: 'fly',
             frames: this.anims.generateFrameNumbers('flyEnemy'),
             frameRate: 20,
+            yoyo: true,
             repeat: -1
         };
 
         let snakeConfig = {
             key: 'move',
             frames: this.anims.generateFrameNumbers('snake'),
-            frameRate: 20,
+            frameRate: 2,
             repeat: -1
         };
 
         this.anims.create(playerConfig);
-        this.anims.create(starConfig);
         this.anims.create(coinConfig);
         this.anims.create(platformConfig);
         this.anims.create(flyConfig);
