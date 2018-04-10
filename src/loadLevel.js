27168396f51dea532f33ffa529cb1b6893dc7497
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-10 19:41:22 +1000

    Load all levels in the cache as Level Objects

--- src/loadLevel.js
+++ src/loadLevel.js
@@ -10,10 +10,18 @@ class LoadLevels extends Phaser.Scene {
 
     create () {
         this.levels = [];
-        let numLevels = this.cache.tilemap.entries.size;
-        let keys = Object.keys(this.cache.tilemap.entries.entries);
+        let map = this.cache.tilemap.entries;
+        let numLevels = map.size;
+        let data = map.entries;
+        let keys = Object.keys(data);
+        let defaultProps = {
+            starTime: 30,
+            devAllStarTime: null,
+            devAnyStarTime: null
+        };
         for (let i = 0; i < numLevels; i++) {
-            this.levels[i] = new Level(keys[i], 5, 3, 3);
+            let properties = data[keys[i]].data.properties || defaultProps;
+            this.levels[i] = new Level(keys[i], properties.starTime, properties.devAllStar, properties.devAnyTime);
         }
         console.log(this.levels);
     }
