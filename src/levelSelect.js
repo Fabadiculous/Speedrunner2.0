089e3ff789aab743ba898313399cbf229496eaf2
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-20 17:44:25 +1000

    Create level select scene

--- src/levelSelect.js
+++ src/levelSelect.js
@@ -1,5 +1,34 @@
-class LevelSelect extends Phaser.Scene {
+import Button from './Button';
 
+class LevelSelect extends Phaser.Scene {
+    constructor () {
+        super({
+            key: 'levelSelect',
+            plugins: [ 'InputPlugin' ]
+        });
+    }
+
+    create () {
+        let menu = this.scene.get('menu');
+        let backBtn = new Button(0, 0, menu.nextScene, [ 'menu' ], this, 'Back');
+        backBtn.setOrigin(0);
+        backBtn.setDimensions(50,50);
+        backBtn.addText('Back');
+
+
+        let lvlSelectTitle = {
+            x: this.registry.get('width') / 2,
+            y: 0,
+            text: 'LEVEL SELECT',
+            style: {
+                fontSize: '64px',
+                fontFamily: 'Arial',
+                color: '#FFD700',
+                align: 'center'
+            }
+        };
+        this.make.text(lvlSelectTitle).setOrigin(0.5, 0);
+    }
 }
 
-export default class LevelSelect;
+export default LevelSelect;
