8661f98e9ef40f37e6262123d02df3cefa9cf706
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-22 06:52:27 +1000

    Update to use menuUI for title and back button

--- src/levelSelect.js
+++ src/levelSelect.js
@@ -8,26 +8,30 @@ class LevelSelect extends Phaser.Scene {
         });
     }
 
+    init () {
+        let menuUI = this.scene.get('menuUI');
+        menuUI.setTitle('LEVEL SELECT');
+        menuUI.addBackBtn(this);
+    }
+
     create () {
-        let menu = this.scene.get('menu');
-        let backBtn = new Button(0, 0, menu.nextScene, [ 'menu' ], this, 'Back');
-        backBtn.setOrigin(0);
-        backBtn.setDimensions(50,50);
-    //    backBtn.addText('Back');
+        // let menu = this.scene.get('menu');
+        // let backBtn = new Button(0, 0, menu.nextScene, [ 'menu' ], this, 'Back');
+        // backBtn.setOrigin(0);
+        // backBtn.setDimensions(50,50);
 
-
-        let lvlSelectTitle = {
-            x: this.registry.get('width') / 2,
-            y: 0,
-            text: 'LEVEL SELECT',
-            style: {
-                fontSize: '64px',
-                fontFamily: 'Arial',
-                color: '#FFD700',
-                align: 'center'
-            }
-        };
-        this.make.text(lvlSelectTitle).setOrigin(0.5, 0);
+        // let lvlSelectTitle = {
+        //     x: this.registry.get('width') / 2,
+        //     y: 0,
+        //     text: 'LEVEL SELECT',
+        //     style: {
+        //         fontSize: '64px',
+        //         fontFamily: 'Arial',
+        //         color: '#FFD700',
+        //         align: 'center'
+        //     }
+        // };
+        // this.make.text(lvlSelectTitle).setOrigin(0.5, 0);
     }
 }
 
