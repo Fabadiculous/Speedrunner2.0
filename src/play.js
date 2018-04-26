1613f6bdebce91407d97fdad6318099e263340c1
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-26 14:34:27 +1000

    Update play scene to use level data passed to it

--- src/play.js
+++ src/play.js
@@ -1,10 +1,21 @@
 class PlayGame extends Phaser.Scene {
     constructor () {
-        super({ key: 'playGame'});
+        super({
+            key: 'playGame',
+            plugins: [ 'InputPlugin' ]
+        });
+    }
+
+    init (data) {
+        let menuUI = this.scene.get('menuUI');
+        menuUI.setTitle('');
+        menuUI.removeBackBtn();
+        this.level = data;
+        console.log(this.level);
     }
 
     create () {
-        this.map = this.make.tilemap({ key: 'tutorial' });
+        this.map = this.make.tilemap({ key: this.level.key });
 
         let tiles = this.map.addTilesetImage('groundTiles');
 
