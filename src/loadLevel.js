c19f1dcf19590789aff729ab4c96ac5e98c5e1c7
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-04-26 14:33:47 +1000

    Sort the levels array, so it is in order

--- src/loadLevel.js
+++ src/loadLevel.js
@@ -11,6 +11,9 @@ class LoadLevels extends Phaser.Scene {
     create () {
         this.levels = [];
         let map = this.cache.tilemap.entries;
+
+        console.log(map);
+
         let numLevels = map.size;
         let data = map.entries;
         let keys = Object.keys(data);
@@ -23,7 +26,19 @@ class LoadLevels extends Phaser.Scene {
             let properties = data[keys[i]].data.properties || defaultProps;
             this.levels[i] = new Level(keys[i], properties.starTime, properties.devAllStar, properties.devAnyTime);
         }
-
+        console.log(this.levels);
+        this.levels.sort((a,b) => {
+            let keyA = a.key;
+            let keyB = b.key;
+            if(keyA < keyB) {
+                return -1;
+            }
+            if (keyA > keyB) {
+                return 1;
+            }
+            return 0;
+        });
+        this.levels.unshift(this.levels.pop());
         this.registry.set('levels', this.levels);
         this.scene.remove('loadLevels');
     }
