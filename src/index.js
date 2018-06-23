6c1ba2864734b31b474e428d82da747fd2c27109
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-06-23 14:58:17 +1000

    Refactor scaling so it works. No need for initalise.js now

--- src/index.js
+++ src/index.js
@@ -10,31 +10,49 @@ import LoadLevels from './loadLevel';
 import LevelSelect from './levelSelect';
 import MenuUI from './menuUI';
 import Options from './options';
-import initialise from './initialise';
 import Config from './config';
 
-const config = {
-    type: Phaser.AUTO,
-    width: Config.DEFAULT_WIDTH,
-    height: Config.DEFAULT_HEIGHT,
-    backgroundColor: '#87CEEB',
-    pixelArt: true,
-    parent: 'gameDiv',
-    scene: [ Preload, Menu, PlayGame, Background, Help, CreateAnims, LoadLevels,
-        LevelSelect, MenuUI, Options ],
-    title: 'Speedrunner 2.0',
-    banner: {
-        text: '#191970',
-        background: [
-            '#555555',
-            '#20B2AA',
-            '#9ACD32',
-            '#B8860B',
-            '#F5F5DC'
-        ],
-        hidePhaser: true
+window.onload = function () {
+    const config = {
+        type: Phaser.AUTO,
+        width: Config.DEFAULT_WIDTH,
+        height: Config.DEFAULT_HEIGHT,
+        backgroundColor: '#87CEEB',
+        pixelArt: true,
+        parent: 'gameDiv',
+        scene: [ Preload, Menu, PlayGame, Background, Help, CreateAnims, LoadLevels,
+            LevelSelect, MenuUI, Options ],
+        title: 'Speedrunner 2.0',
+        banner: {
+            text: '#191970',
+            background: [
+                '#555555',
+                '#20B2AA',
+                '#9ACD32',
+                '#B8860B',
+                '#F5F5DC'
+            ],
+            hidePhaser: true
+        }
+    };
+
+    const game = new Phaser.Game(config);
+    resize();
+    window.addEventListener('resize', resize, false);
+
+    function resize () {
+        let canvas = document.querySelector(`#${game.config.parent} canvas`);
+        console.log(canvas);
+        let windowWidth = window.innerWidth;
+        let windowHeight = window.innerHeight;
+        let windowRatio = windowWidth / windowHeight;
+        let gameRatio = game.config.width / game.config.height;
+        if(windowRatio < gameRatio) {
+            canvas.style.width = `${windowWidth}px`;
+            canvas.style.height = `${windowHeight / gameRatio}px`;
+        } else{
+            canvas.style.width = `${windowHeight * gameRatio}px`;
+            canvas.style.height = `${windowHeight}px`;
+        }
     }
 };
-
-const game = new Phaser.Game(config);
-initialise(game);
