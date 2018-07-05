22c46e75091f3ba37046e4b7e06a850e04c0df26
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-07-05 12:31:30 +1000

    Add a pause menu

--- src/pauseMenu.js
+++ src/pauseMenu.js
@@ -10,11 +10,26 @@ class PauseMenu extends Phaser.Scene {
         this.input.keyboard.on('keydown_P', () => {
             this.unpause();
         });
-        let menu = this.add.nineslice(100, 100, 600, 400, 'button', 2);
-        let resume = this.add.text(menu.x + menu.displayWidth / 2, menu.y + menu.displayHeight / 2, 'Resume');
+        let buttonConfig = {
+            key: 'button',
+            frame: 2
+        };
+
+        let menuTextStyle = {
+            font: '24px Arial',
+            color: '#ffffff',
+            align: 'center'
+        };
+        let menu = this.add.nineslice(100, 100, 600, 400, buttonConfig, 2);
+        let resume = this.add.text(menu.x + menu.displayWidth / 2, menu.y + menu.displayHeight / 4, 'Resume', menuTextStyle);
         resume.setInteractive();
         resume.setOrigin(0.5);
         resume.on('pointerdown', () => this.unpause());
+
+        let returnToMenu = this.add.text(menu.x + menu.displayWidth / 2, menu.y + 3 * menu.displayHeight / 4, 'Return To Menu', menuTextStyle);
+        returnToMenu.setInteractive();
+        returnToMenu.setOrigin(0.5);
+        returnToMenu.on('pointerdown', () => this.returnToMenu());
     }
 
     update () {
@@ -24,6 +39,13 @@ class PauseMenu extends Phaser.Scene {
     unpause () {
         this.scene.resume('playGame');
         this.scene.stop();
+    }
+
+    returnToMenu () {
+        this.scene.stop();
+        this.scene.stop('playGame');
+        this.scene.start('menu');
+        this.scene.launch('menuUI');
     }
 }
 
