5c746bd11eb372cce83c4a4387ae6451681a1b60
Daniel <24710205+Fabadiculous@users.noreply.github.com>
2018-07-24 14:36:18 +1000

    Add controls to config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,13 @@
 const Config = {
     DEFAULT_WIDTH: 800,
-    DEFAULT_HEIGHT: 600
+    DEFAULT_HEIGHT: 600,
+    controls: {
+        jump: 'UP',
+        right: 'RIGHT',
+        left: 'LEFT',
+        run: 'SHIFT',
+        restart: 'R'
+    }
 };
 
 export default Config;
