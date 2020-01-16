82aeb8a67957dabc0eb92e8533dc9ade8fa01cf1
Fabadiculous <24710205+Fabadiculous@users.noreply.github.com>
2020-01-16 14:22:53 +1000

    Implement moving left and right with timesteps

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,5 @@
 class Player extends Phaser.Physics.Arcade.Sprite {
-    constructor (scene, x, y, controls) {
+    constructor(scene, x, y, controls) {
         super(scene, x, y, 'player', 0);
         scene.add.existing(this);
         scene.physics.world.enable(this);
@@ -8,7 +8,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         this.JUMP = 9; // The frame when the player jumps
 
         this.TILESIZE = 32; // Width of a tile in the map in pixels
-        this.XSPEED = 5; // Horizontal speed in tiles per second
+        this.XSPEED = Phaser.Math.GetSpeed(5 * this.TILESIZE, 1); // Horizontal speed in tiles per second
         this.PEAKHEIGHT = 1.5; // Maximum height of a jump in tiles
         this.PEAKDISTANCE = 3; // The horizontal distance travelled to reach the peak of the jump
         this.PEAKTIME = this.XSPEED / this.JUMPHEIGHT; // Time to peak of jump in ms
@@ -19,50 +19,57 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         scene.input.keyboard.addCapture(Object.values(controls));
         this.controls = scene.input.keyboard.addKeys(controls);
         this.setCollideWorldBounds(true);
-        this.setGravityY(-this.gravity * this.TILESIZE);
+        this.body.setSize(14, 32);
+        this.body.setOffset(-1, 0);
 
+        // this.setGravityY(this.gravity * this.TILESIZE);
+        this.setGravityY(1000);
         this.jumping = false;
     }
 
-    update () {
+    preUpdate(time, delta) {
+
+        if (this.controls.run.isDown) {
+            this.running = 0.25;
+        } else {
+            this.running = 0;
+        }
+
         if (this.controls.right.isDown) {
+            this.setScale(1, 1);
+            console.log(this.body.velocity.x);
+
+            this.body.velocity.x = this.XSPEED + this.running;
+            this.x += this.body.velocity.x * delta;
             if (this.body.velocity.y === 0) {
                 if (!this.anims.isPlaying) {
-                    this.anims.play('run');
+                    this.play('run');
                 }
-            }
-            this.setFlipX(false);
-            if (this.controls.run.isDown) {
-                this.setVelocityX(this.XSPEED * this.TILESIZE * 2);
-            } else {
-                this.setVelocityX(this.XSPEED * this.TILESIZE);
             }
         } else if (this.controls.left.isDown) {
+            this.setFlipX(true);
+            this.body.velocity.x = -(this.XSPEED + this.running);
+            this.x += this.body.velocity.x * delta;
             if (this.body.velocity.y === 0) {
-            
                 if (!this.anims.isPlaying) {
                     this.anims.play('run');
                 }
-           
-            }
-            this.setFlipX(true);
-            if (this.controls.run.isDown) {
-                this.setVelocityX(-this.XSPEED * this.TILESIZE * 2);
-            } else {
-                this.setVelocityX(-this.XSPEED * this.TILESIZE);
             }
         } else {
             this.setVelocityX(0);
+        }
+
+        if (this.body.velocity.x === 0) {
             this.anims.stop();
             this.setFrame(this.IDLE);
         }
 
-        if (this.controls.jump.isDown && this.body.onFloor()) {
-            this.jump();
-        }
+        // if (this.controls.jump.isDown && this.body.onFloor()) {
+        //     this.jump();
+        // }
     }
 
-    jump () {
+    jump() {
         if (!this.jumping) {
             this.jumping = true;
             this.anims.stop();
